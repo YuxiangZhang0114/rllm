14c5b9e8427d42b42b32adfadae441688ee91f02
shujiangming <shujiangming@myhexin.com>
2026-01-08 22:49:37 -0800

    Update training scripts for GRPO and distillation with enhanced configurations
    - Increased batch sizes and response lengths in `train_search_distill.sh` and `train_search_grpo.sh` to improve training efficiency.
    - Added new parameters for handling overlong prompts and raw chat returns.
    - Adjusted rollout settings, including the number of samples and maximum token lengths, to optimize performance.
    
    These changes aim to enhance the training process and improve the model's handling of input data.

--- examples/search/train_search_grpo.sh
+++ examples/search/train_search_grpo.sh
@@ -11,21 +11,27 @@ export VLLM_ALLOW_LONG_MAX_MODEL_LEN=1
 export VLLM_ENGINE_ITERATION_TIMEOUT_S=100000000000
 export CUDA_VISIBLE_DEVICES=0,1,2,3
 
+export WANDB_API_KEY="0559d52399bc5d3fd8e373bb4b8b6e8db054b9f7"
+
 # 运行训练脚本
 python3 -m examples.search.train_search_grpo \
     algorithm.adv_estimator=grpo \
     data.train_batch_size=128 \
     data.val_batch_size=400 \
     data.max_prompt_length=2048 \
-    data.max_response_length=2048 \
+    data.max_response_length=20480 \
+    data.filter_overlong_prompts=True \
+    data.truncation='error' \
+    data.return_raw_chat=True \
     actor_rollout_ref.model.path=Qwen/Qwen3-4B-Instruct-2507 \
     actor_rollout_ref.hybrid_engine=True \
     actor_rollout_ref.actor.optim.lr=1e-6 \
     actor_rollout_ref.model.use_remove_padding=True \
     actor_rollout_ref.actor.loss_agg_mode=seq-mean-token-sum \
-    actor_rollout_ref.actor.ppo_mini_batch_size=32 \
+    actor_rollout_ref.actor.ppo_mini_batch_size=64 \
+    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=4 \
     actor_rollout_ref.actor.use_dynamic_bsz=True \
-    actor_rollout_ref.actor.ppo_max_token_len_per_gpu=24000 \
+    actor_rollout_ref.actor.ppo_max_token_len_per_gpu=23000 \
     actor_rollout_ref.actor.use_kl_loss=False \
     actor_rollout_ref.actor.clip_ratio_high=0.28 \
     actor_rollout_ref.actor.kl_loss_coef=0.001 \
@@ -38,6 +44,7 @@ python3 -m examples.search.train_search_grpo \
     actor_rollout_ref.rollout.name=vllm \
     actor_rollout_ref.rollout.mode="async" \
     actor_rollout_ref.rollout.enforce_eager=False \
+    actor_rollout_ref.rollout.max_model_len=23000 \
     actor_rollout_ref.rollout.temperature=0.7 \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.85 \
     actor_rollout_ref.rollout.n=5 \
@@ -46,8 +53,12 @@ python3 -m examples.search.train_search_grpo \
     actor_rollout_ref.rollout.val_kwargs.top_p=0.8 \
     actor_rollout_ref.rollout.val_kwargs.top_k=20 \
     actor_rollout_ref.ref.fsdp_config.param_offload=True \
-    actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=1 \
-    actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=1 \
+    actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=4 \
+    actor_rollout_ref.ref.log_prob_use_dynamic_bsz=True \
+    actor_rollout_ref.ref.log_prob_max_token_len_per_gpu=32768 \
+    actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=4 \
+    actor_rollout_ref.rollout.log_prob_use_dynamic_bsz=True \
+    actor_rollout_ref.rollout.log_prob_max_token_len_per_gpu=32768 \
     actor_rollout_ref.actor.entropy_coeff=0 \
     algorithm.kl_ctrl.kl_coef=0.001 \
     rllm.mask_truncated_samples=False \
